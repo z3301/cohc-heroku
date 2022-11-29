2f86bb2bccc5c643d449f0ab4f6f4ea9c61e3882
z3301 <98485231+z3301@users.noreply.github.com>
2022-11-28 21:39:55 -0500

    fixing cors

--- server.js
+++ server.js
@@ -28,7 +28,8 @@ app.use(cookieParser());
 app.use(cors({
   origin: ['https://cohc.onrender.com', 'https://cohc-server.onrender.com',
   'http://cohc.onrender.com', 'http://cohc-server.onrender.com',
-  'http://localhost:3000', 'http://localhost:3009'],
+  'http://localhost:3000', 'http://localhost:3009',
+  'https://cohc.herokuapp.com/'],
   credentials: true, // enable set cookie
 }));
 
